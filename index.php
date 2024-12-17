e6af8902a2ef93911ac182602fed44a0c0571ef2
slim <slimabida21@gmail.com>
2024-12-17 23:43:55 +0100

    creation du dossier template pour la configuration des pages du site avec header.php footer.php et listing_part.php pour créé des cartes des dernères annonces

--- index.php
+++ index.php
@@ -1,32 +1,49 @@
-<!DOCTYPE html>
-<html lang="fr">
+<!-- ###### Secrtion heros -->
+<?php require_once './template/header.php';
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Okaz</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="./Assets/css/override-bootstrap.css">
+$listings = [
 
-</head>
-
-<body>
-
-
-    
+    ["title" => "test1"],
+    ["title" => "test2"],
+    ["title" => "test3"],
+];
 
 
 
 
+?>
+
+<div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+    <div class="col-10 col-sm-8 col-lg-6">
+        <img src="Assets/image/logo-okaz.png" class="d-block mx-lg-auto img-fluid" alt="" width="400" loading="lazy">
+    </div>
+    <div class="col-lg-6">
+        <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Avec Okaz achetez et vendez vos objets</h1>
+        <p class="lead">Trouvez ce que vous cherchez ou donnez une seconde vie à vos objets en un clic ! Okaz est la plateforme incontournable pour vendre, acheter ou échanger tout ce que vous souhaitez : vêtements, meubles, appareils électroniques, véhicules et bien plus encore !</p>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+            <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+            <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+        </div>
+    </div>
+</div>
+<!-- Section Annonces Cartes  -->
+<div class="container">
+    <div class="row text-center">
+        <h2>Les dernières annonces</h2>
+        <div class="row justify-content-center g-4 mt-3">
+            <?php
+
+            foreach ($listings as $listing) {
+                require './template/listing_part.php';
+            }
 
 
 
+            ?>
+        </div>
+    </div>
+</div>
 
 
 
-
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-
-</body>
-
-</html>
+<?php require_once './template/footer.php'; ?>
---
+++ template/footer.php
@@ -0,0 +1,20 @@
+</main>
+</div>
+
+<!-- Le footer ###### -->
+
+<footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
+    </ul>
+    <p class="text-center text-body-secondary">© 2024 Company, Inc</p>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
