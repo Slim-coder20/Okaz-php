2440920a886045c61b8b5da8f4e1762d511a1307
slim <slimabida21@gmail.com>
2024-12-18 09:30:21 +0100

    upoload dossier image pour la mise en place des cartes

--- index.php
+++ index.php
@@ -3,13 +3,11 @@
 
 $listings = [
 
-    ["title" => "test1"],
-    ["title" => "test2"],
-    ["title" => "test3"],
+    ["title" => "test1", "price"=>30, "image" => "rocket-league.jpg"],
+    ["title" => "test2", "price"=>25, "image" => "rocket-league.jpg"],
+    ["title" => "test3", "price"=>15, "image" => "rocket-league.jpg"],
+    
 ];
-
-
-
 
 ?>
 
--- template/listing_part.php
+++ template/listing_part.php
@@ -1,10 +1,10 @@
 <div class="col-md-4 d-flex justify-content-center">
     <div class="card">
-        <img src="..." class="card-img-top" alt="...">
+        <img src="../uploads/listing/rocket-league.jpg" class="card-img-top" alt="<?= $listing["title"] ?>">
         <div class="card-body">
-            <h5 class="card-title"><?= $listing ["title"]?></h5>
+            <h5 class="card-title"><?= $listing["image"] ?></h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <a href="#" class="btn btn-primary">consultez</a>
         </div>
     </div>
 </div>
